f68d40685679b52f740c44b03b15474e4a23da2c
Mehdi Bizhani <mbizhani@gmail.com>
2022-09-11 00:20:48 +0430

    update templates due to latest version 2 changes

--- artemis-core/src/main/resources/_template_.groovy
+++ artemis-core/src/main/resources/_template_.groovy
@@ -1,51 +1,65 @@
-import org.devocative.artemis.Context
+import org.devocative.artemis.ctx.InitContext
 
-/*
+/**
   Called at the startup time
 */
-def before(Context ctx) {
+def before(InitContext init) {
 	/*
-	You can call any initialization statements here, such as:
+	  You can initialize your tests, such as:
 
-	Artemis.log("...")
-	ctx.addVar(<VAR>, <VAR_VALUE>)
+	  Artemis.log("...")
+	  init.ctx.addVar(<VAR>, <VAR_VALUE>)
 
-	// An interceptor before sending any request to server
-	ctx.config.beforeSend = { HttpRequestData rqData ->
-		rqData.headers["<HEADER_KEY>"] = <HEADER_VALUE>
-		// alter sending request by setting data to 'rqData' properties
-	}
-	*/
+	  // An interceptor before sending any request to server
+	  init.aspects.createBeforeSend({
+		  // `it` is `org.devocative.artemis.ctx.BeforeSendData`
+
+		  it.data.headers["<HEADER_KEY>"] = <HEADER_VALUE>
+
+		  switch (it.data.method) {
+			  case "POST":
+			  break;
+			  case "PUT":
+			  break
+			  case "GET":
+			  break
+		  }
+	  })
+
+	  init.aspects.createCommonAssertRs({
+		  // `it` is `org.devocative.artemis.ctx.CommonAssertRs`
+		  it
+			  .matches("<REG_EX>", {
+				  // it.rqId, it.ctx.vars
+			  })
+			  .other({
+			  })
+	  })
+	  */
 }
 
+
 /*
-  A sample function, callable in XML file as ${_.generate(5, '1'..'9')}
-*/
-def generate(int n, List<String>... alphaSet) {
-	def list = alphaSet.flatten()
-	new Random().with {
-		(1..n).collect { list[nextInt(list.size())] }.join()
-	}
+- You can call following function in your XML file by ${_.FUNC1(...)} groovy expression
+
+def FUNC1(...) {
+	...
 }
 
-// ------------------------------
 
-/*
-
- // You can set `call="true"` in the XML request tag, and then create a function
- // with the same name as its id. This function is called before sending the request.
+- By setting `call="true"` in scenario or request, following function is called before:
 
 def <ID_VALUE>(Context ctx) {
 	// Here you can call `ctx.addVar()` to add variables to the context, and use them later.
 }
 
 
-  // To validate the response body of a request, just set `call="true"` on the <assertRs/> tag,
-  // and create a function called `assertRs_<ID_VALUE>`. The first parameter is the `Context` object,
-  // and the second parameter is the response body, which is type of `Map` or `List`.
-  // Add JUnit dependency, or other assertion libraries, to validate the response body.
+- To validate the response body of a request, just set `call="true"` on the <assertRs/> tag,
+  and create a function called `assertRs_<ID_VALUE>`. The first parameter is the `Context` object,
+  and the second parameter is the response body, which is type of `Map` or `List`.
+  Add JUnit dependency, or other assertion libraries, to validate the response body.
 
-  // If the response body is an object
+  - if the response body is an object
 def assertRs_<ID_VALUE>(Context ctx, Map responseBody){
 	// For example:
 	// Assertions.assertEquals(ctx.vars.cell, responseBody.cell)
@@ -53,7 +67,7 @@ def assertRs_<ID_VALUE>(Context ctx, Map responseBody){
 	// Assertions.assertNotNull(responseBody.userId)
 }
 
-  // If the response body is an array
+  - if the response body is an array
 def assertRs_<ID_VALUE>(Context ctx, List responseBody){
 }
 
