f8b72d7864284dc5c130fc401975854c91acf9b7
mbizhani <mbizhani@gmail.com>
2023-01-16 20:54:11 +0330

    - refactor: support both https & https request type and via any proxy - for https, allows any cert

--- artemis-core/src/main/java/org/devocative/artemis/http/HttpFactory.java
+++ artemis-core/src/main/java/org/devocative/artemis/http/HttpFactory.java
@@ -2,21 +2,26 @@ package org.devocative.artemis.http;
 
 import org.apache.hc.client5.http.classic.methods.HttpUriRequestBase;
 import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.apache.hc.client5.http.impl.classic.HttpClients;
 import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManager;
 import org.apache.hc.client5.http.impl.routing.DefaultProxyRoutePlanner;
-import org.apache.hc.client5.http.routing.HttpRoutePlanner;
 import org.apache.hc.client5.http.socket.ConnectionSocketFactory;
 import org.apache.hc.client5.http.socket.PlainConnectionSocketFactory;
+import org.apache.hc.client5.http.ssl.NoopHostnameVerifier;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
 import org.apache.hc.core5.http.HttpHost;
 import org.apache.hc.core5.http.config.Registry;
 import org.apache.hc.core5.http.config.RegistryBuilder;
 import org.apache.hc.core5.http.protocol.HttpContext;
 import org.apache.hc.core5.net.URIBuilder;
+import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.core5.ssl.TrustStrategy;
 import org.devocative.artemis.TestFailedException;
 import org.devocative.artemis.log.ALog;
 import org.devocative.artemis.xml.XBaseRequest;
 
+import javax.net.ssl.SSLContext;
 import java.io.IOException;
 import java.net.*;
 
@@ -74,36 +79,26 @@ public class HttpFactory {
 
 		final CloseableHttpClient httpclient;
 		if (CURRENT_CLIENT.get() == null) {
-			if (proxy != null) {
-				final String[] scheme_other = proxy.split("://");
-				final String proxyScheme = scheme_other[0];
-				final String[] host_port = scheme_other[1].split(":");
-				final String proxyHost = host_port[0];
-				final int proxyPort = Integer.parseInt(host_port[1]);
+			final SocketProxy socketProxy = new SocketProxy(proxy);
 
-				if ("http".equals(proxyScheme)) {
-					final HttpRoutePlanner proxyRoutePlanner = new DefaultProxyRoutePlanner(new HttpHost(proxyScheme, proxyHost, proxyPort));
-					httpclient = HttpClients
-						.custom()
-						.setRoutePlanner(proxyRoutePlanner)
-						.build();
-				} else if ("socks".equals(proxyScheme)) {
-					final Registry<ConnectionSocketFactory> reg = RegistryBuilder
-						.<ConnectionSocketFactory>create()
-						.register("http", new ProxiedConnectionSocketFactory(proxyHost, proxyPort))
-						.build();
+			final Registry<ConnectionSocketFactory> reg = RegistryBuilder
+				.<ConnectionSocketFactory>create()
+				.register("http", new ProxiedConnectionSocketFactory(socketProxy))
+				.register("https", new ProxiedSslConnectionSocketFactory(socketProxy))
+				.build();
 
-					final PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(reg);
-					httpclient = HttpClients
-						.custom()
-						.setConnectionManager(cm)
-						.build();
-				} else {
-					throw new RuntimeException("Invalid proxy scheme: " + proxyScheme);
-				}
-			} else {
-				httpclient = HttpClients.createDefault();
+			final PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager(reg);
+
+			final HttpClientBuilder clientBuilder = HttpClients
+				.custom()
+				.setConnectionManager(cm);
+
+			if (socketProxy.isSet() && socketProxy.isHttp()) {
+				clientBuilder.setRoutePlanner(new DefaultProxyRoutePlanner(socketProxy.createHttpProxy()));
 			}
+
+			httpclient = clientBuilder.build();
+
 			CURRENT_CLIENT.set(httpclient);
 		} else {
 			httpclient = CURRENT_CLIENT.get();
@@ -114,21 +109,96 @@ public class HttpFactory {
 
 	// ------------------------------
 
-	static class ProxiedConnectionSocketFactory extends PlainConnectionSocketFactory {
-		private final String proxyHost;
-		private final int proxyPort;
+	static class SocketProxy {
+		private final String scheme;
+		private final String host;
+		private final int port;
 
-		public ProxiedConnectionSocketFactory(String proxyHost, int proxyPort) {
-			this.proxyHost = proxyHost;
-			this.proxyPort = proxyPort;
+		public SocketProxy(String proxy) {
+			if (proxy != null) {
+				final String[] scheme_other = proxy.split("://");
+				this.scheme = scheme_other[0].toLowerCase();
+				final String[] host_port = scheme_other[1].split(":");
+				this.host = host_port[0];
+				this.port = Integer.parseInt(host_port[1]);
+			} else {
+				this.scheme = null;
+				this.host = null;
+				this.port = -1;
+			}
+		}
+
+		public boolean isSet() {
+			return host != null;
+		}
+
+		public boolean isSocks() {
+			return "socks".equals(scheme);
+		}
+
+		public boolean isHttp() {
+			return "http".equals(scheme);
+		}
+
+		public Proxy createSocksProxy() {
+			final InetSocketAddress socksAddr = new InetSocketAddress(host, port);
+			return new Proxy(Proxy.Type.SOCKS, socksAddr);
+		}
+
+		public HttpHost createHttpProxy() {
+			return new HttpHost(scheme, host, port);
 		}
 
 		@Override
-		public Socket createSocket(HttpContext context) {
-			ALog.info("ProxiedConnectionSocketFactory: socks={}:{}", proxyHost, proxyPort);
-			final InetSocketAddress socksAddr = new InetSocketAddress(proxyHost, proxyPort);
-			final Proxy proxy = new Proxy(Proxy.Type.SOCKS, socksAddr);
-			return new Socket(proxy);
+		public String toString() {
+			return String.format("%s://%s:%s", scheme, host, port);
+		}
+	}
+
+	static class ProxiedConnectionSocketFactory extends PlainConnectionSocketFactory {
+		private final SocketProxy socketProxy;
+
+		public ProxiedConnectionSocketFactory(SocketProxy socketProxy) {
+			this.socketProxy = socketProxy;
+		}
+
+		@Override
+		public Socket createSocket(HttpContext context) throws IOException {
+			if (socketProxy.isSet() && socketProxy.isSocks()) {
+				ALog.info("ProxiedConnectionSocketFactory: {}", socketProxy);
+				return new Socket(socketProxy.createSocksProxy());
+			} else {
+				return super.createSocket(context);
+			}
+		}
+	}
+
+	static class ProxiedSslConnectionSocketFactory extends SSLConnectionSocketFactory {
+		private final SocketProxy socketProxy;
+
+
+		public ProxiedSslConnectionSocketFactory(SocketProxy socketProxy) {
+			super(createSslSocketFactory(), NoopHostnameVerifier.INSTANCE);
+			this.socketProxy = socketProxy;
+		}
+
+		@Override
+		public Socket createSocket(HttpContext context) throws IOException {
+			if (socketProxy.isSet() && socketProxy.isSocks()) {
+				ALog.info("ProxiedSslConnectionSocketFactory: {}", socketProxy);
+				return new Socket(socketProxy.createSocksProxy());
+			} else {
+				return super.createSocket(context);
+			}
+		}
+
+		private static SSLContext createSslSocketFactory() {
+			try {
+				final TrustStrategy acceptingTrustStrategy = (cert, authType) -> true;
+				return SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
 		}
 	}
 }
